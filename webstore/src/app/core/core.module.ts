d755fb0dce5bf8430e36447bb25579d0bab93599
nikitausaty <nikitausatiy@yandex.ru>
2021-09-02 05:53:19 +0300

    feat: create basic header-top layout

---
+++ webstore/src/app/core/core.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderTopComponent } from './header/header-top/header-top.component';
+
+
+
+@NgModule({
+  declarations: [
+    HeaderTopComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CoreModule { }
