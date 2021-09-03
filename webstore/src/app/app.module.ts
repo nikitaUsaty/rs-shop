17dd431f64f12312d933bbacd2dd589d91087804
nikitausaty <nikitausatiy@yandex.ru>
2021-09-03 04:10:10 +0300

    feat: add location service

--- webstore/src/app/app.module.ts
+++ webstore/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderTopComponent } from './core/components/header/header-top/header-top.component';
+import { HttpClientModule } from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CoreModule],
+  imports: [BrowserModule, AppRoutingModule, CoreModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- webstore/src/app/core/components/header/header-top/header-top.component.ts
+++ webstore/src/app/core/components/header/header-top/header-top.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { IGeoAPI } from 'src/app/core/models/geo-api.interface';
+
+import { LocationService } from 'src/app/core/services/location.service';
 
 @Component({
   selector: 'app-header-top',
@@ -6,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header-top.component.scss'],
 })
 export class HeaderTopComponent implements OnInit {
-  constructor() {}
+  constructor(private service: LocationService) {}
 
-  ngOnInit(): void {}
+  public city!: IGeoAPI;
+
+  ngOnInit(): void {
+    this.service.getLocation().subscribe((val: any) => {
+      this.city = val.city;
+    });
+  }
 }
